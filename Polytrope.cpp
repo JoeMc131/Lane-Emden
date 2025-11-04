214acb344d1990601d41a9f7ed0962798b2369cb
JoeMc131 <joemc131@outlook.com>
2025-11-04 12:08:48 +0000

    changing polytrope class

--- Polytrope.cpp
+++ Polytrope.cpp
@@ -6,12 +6,28 @@
 using namespace std;
 using namespace CONSTANTS;
 
+
+vector<double> dy_dx(double xi, vector<double> y, double n){
+
+    double beta, theta = y[0], y[1];
+
+    return {-(2*beta)/xi - pow(theta, n), beta};
+}
+
 class Polytrope{
 public:
     double index;
-    vector<double> xi;
+    double mass;
+    vector<double> beta;
     vector<double> theta;
     vector<double> pressure;
     vector<double> temperature;
     vector<double> density;
+
+
+    Polytrope(double n, double M){
+        mass = M;
+        index = n;
+
+    }
 };
